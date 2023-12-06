f44878b0fc6fbbdf07051ba55d6614e93dffc92d
Yahir <yahir@example.com>
2023-12-06 14:20:52 -0600

    Codigo de inicilizacion

---
+++ Inicio.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+int main()
+{
+char inicio = 'x';
+int magia = 4;
+int arma = 0;
+char tecla;
+int eleccion;
+char final;
+int pregunta = 0;
+    cout<<"Bienvenido \nEres un usuario de la magia, sin embargo como novato debes pasar la prueba que definirá si eres apto"<<endl;
+    do{cout<<"Presiona un x para iniciar"; 
+    cin>>tecla;
+    
+    }while(inicio!=tecla);
+    
+  
+    return 0;}
